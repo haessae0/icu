c6824a7f432561426ec98f7c170021864016a8aa
haessae0 <haessae0@gmail.com>
2022-05-11 20:47:34 +0900

    created Gradle & Vue directories

---
+++ ICU/src/main/java/com/icu/IcuApplication.java
@@ -0,0 +1,13 @@
+package com.icu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcuApplication.class, args);
+    }
+
+}
