91fafeb29db6aae550e11d205f72c7375af978b4
rlatnqls11 <solvit768@naver.com>
2022-05-18 22:06:30 +0900

    server.py 수정ㅂ

---
+++ ICU_flask/camera.py
@@ -0,0 +1,90 @@
+import os
+import cv2
+import threading
+from flask import Flask, render_template, Response
+import tensorflow as tf
+import numpy as np
+from yolo_helper import YoloV3, load_darknet_weights, draw_outputs
+import time
+import sys
+import pymysql
+
+yolo = YoloV3()
+load_darknet_weights(yolo, 'yolov3.weights')
+
+class RecordingThread (threading.Thread):
+    def __init__(self, name, camera):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.isRunning = True
+        self.cap = camera 
+        fourcc = cv2.VideoWriter_fourcc(*'avc1') 
+        self.out = cv2.VideoWriter('./static/video.mp4',fourcc, 20.0, (640,480))
+
+    def run(self):
+        while self.isRunning:
+            ret, frame = self.cap.read()
+            if ret:
+                self.out.write(frame)
+        self.out.release()
+    def stop(self):
+        self.isRunning = False
+    def __del__(self):
+        self.out.release()
+
+class VideoCamera(object):
+    def __init__(self,start_time,time_list):
+        # Open a camera
+        self.cap = cv2.VideoCapture(0) 
+        # Initialize video recording environment
+        self.is_record = False
+        self.out = None
+        # Thread for recording
+        self.recordingThread = None
+        self.start_time = start_time
+        self.timelist = time_list
+
+    def __del__(self):
+        self.cap.release()
+    def get_frame(self):
+        while(True):
+            ret, frame = self.cap.read()
+            if ret == False:
+                break
+            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = cv2.resize(img, (320, 320))
+            img = img.astype(np.float32)
+            img = np.expand_dims(img, 0)
+            img = img / 255
+            class_names = [c.strip() for c in open("models/classes.TXT").readlines()]
+            boxes, scores, classes, nums = yolo(img)
+            print("nums:",nums)
+            count=0
+
+            if self.is_record:
+                elapsed_time = time.time() - self.start_time
+            for i in range(nums[0]):
+                temp = classes[0][i]
+                print("nums[i]:",nums[i])
+                if int(classes[0][i] == 0):
+                    count +=1
+                if self.is_record and int(temp==62 or temp == 63 or temp==67 or temp==73): #tvmonitor, laptop, cell phone, book
+                    self.timelist.append(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+            if self.is_record and count > 1: #count != 1: 'No person detected' & 'More than one person detected'
+                self.timelist.append(time.strftime("%H:%M:%S", time.gmtime(elapsed_time)))
+            frame = draw_outputs(frame, (boxes, scores, classes, nums), class_names)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            return buffer.tobytes()
+        else:
+            return None
+    def start_record(self):
+        self.is_record = True
+        self.recordingThread = RecordingThread("Video Recording Thread", self.cap)
+        self.recordingThread.start()
+
+    def stop_record(self):
+        self.is_record = False
+        if self.recordingThread != None:
+            self.recordingThread.stop()
+            
---
+++ ICU_flask/server.py
@@ -0,0 +1,138 @@
+import pymysql
+import time
+
+from flask import Flask, render_template, Response, jsonify, request
+from flask_mysqldb import MySQL
+from camera import VideoCamera,RecordingThread
+
+# Flask 인스턴스 생성
+app = Flask(__name__)
+
+# 데이터베이스 접근(host,user,password,db는 사용자 환경에 맞게 설정할 것)
+conn = pymysql.connect(host='localhost',user='root',password='root',db='ex1',charset='utf8')
+
+v_camera = None
+global_frame = None
+t_list = None
+userid = None
+testnum = None
+
+status = 'true'
+
+# @ : 장식자를 의미하며 URL 연결에 사용
+@app.route('/<input_name>/<input_testnum>')
+def index(input_name, input_testnum):
+
+    # 쿼리문 실행
+    global conn
+
+    query = conn.curssor()
+    query.execute("SELECT * FROM testproblem where exam_num = %s", input_testnum)
+
+    global userid
+    global testnum
+    userid = input_name;
+    testnum = input_testnum;
+
+    # fetchall() : 레코드를 배열 형식으로 저장
+    f_data = query.fetchall()
+    print(len(f_data))
+    p_list = []
+    index = 0
+    
+    for problem in f_data:
+      
+        prob_list = problem[5].split('/')
+
+        p_list.append([])
+        p_list[index].append(prob_list[0]) # 문제보기 0
+        p_list[index].append(prob_list[1]) # 문제보기 1
+        p_list[index].append(prob_list[2]) # 문제보기 2
+        p_list[index].append(prob_list[3]) # 문제보기 3
+        p_list[index].append(problem[2]) # 문제 및 답
+        p_list[index].append(problem[4]) # 문제 및 답
+        
+        index+=1 # 그 다음 문제 출력
+
+    # 쿼리 실행
+    query.execute("SELECT start_time, end_time FROM test WHERE exam_num=%s",testnum)
+
+    f_data2 = query.fetchall()
+    startTime = f_data2[0][0] # 시험 시작 시간
+    endTime = f_data2[0][1] # 시험 끝 시간
+
+    return render_template('index.html', data_list = p_list, u_name = userid, exam_num = testnum, start_time = startTime, end_time = endTime)
+
+
+@app.route('/status', methods=['POST'])
+def status():
+    global v_camera 
+    global t_list
+    global userid
+    global testnum
+    global conn
+
+    if v_camera != None:
+        json = request.get_json()
+        global status 
+        status = json['status']
+    
+        querys = conn.cursor()
+
+        if status == "false":
+            v_camera.stop_record()
+            
+            try :
+                if t_list !=None and userid != None and testnum != None:
+                    tmp_str = ''
+                    
+                    for time in t_list:
+                        tmp_str += time+'/'
+                    
+                    if tmp_str == '':
+                        sql = """UPDATE studenttest SET cheating_time=%s, cheating=%s where s_id=%s and exam_num=%s""" 
+                        querys.execute(sql,('','False',userid,testnum))   
+                    else :
+                        sql = """UPDATE studenttest SET cheating_time=%s, cheating=%s where s_id=%s and exam_num=%s"""
+                        querys.execute(sql,(tmp_str,'True',userid,testnum))
+                    
+                    conn.commit()    
+                    conn.close()
+            except Exception:
+                print(Exception)
+                conn.rollback()    
+                conn.close()
+            return jsonify(result="finished")
+
+@app.route('/video_viewer')
+def video_viewer():
+    return Response(video_stream(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def video_stream(): # 좌측 하단 규격
+    global v_camera 
+    global global_frame
+    global t_list
+
+    start_time = time.time()
+    t_list = list()
+
+    if v_camera == None:
+        v_camera = VideoCamera(start_time,t_list)
+    
+    v_camera.start_record()
+
+    while True:
+        frame = v_camera.get_frame()
+        if frame != None:
+            global_frame = frame
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            yield (b'--frame\r\n'
+                            b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+
+
+# 메인 함수 선언(시작)
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
